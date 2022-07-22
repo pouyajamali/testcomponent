839c4ffdec27fe41cf8765af12977418aedad8cd
Pooya Jamali <pooya.jamali@cocoflo.tech>
2022-07-21 18:26:14 -0700

    first commit

---
+++ TestComponent.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+
+const divStyle = {
+    backgroundColor:"red", 
+    height:"110px", 
+    width:"200px", 
+    marginLeft:"auto", 
+    marginRight: "auto", 
+    marginTop:"200px",
+    paddingTop:"90px",
+  };
+const TestComponent = ({}) => {
+    return (
+        <div style={divStyle}>
+            heybuddyyyyyyyyyyyyyy
+        </div>
+    )
+}
+
+module.export.TestComponent = TestComponent;
